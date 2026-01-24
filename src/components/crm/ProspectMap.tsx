acbc506fe722fea2cfe62aa544b9092d07efdd7e
HugoPortier <hugo_portier@hotmail.com>
2026-01-24 02:43:14 +0100

    change z-index for the map in prospection

--- src/components/crm/ProspectMap.tsx
+++ src/components/crm/ProspectMap.tsx
@@ -288,7 +288,7 @@ export function ProspectMap({
       <div
         ref={mapContainerRef}
         style={{ height }}
-        className={`rounded-lg border overflow-hidden ${className}`}
+        className={`rounded-lg border overflow-hidden ${className} z-0`}
       />
       
       {/* Cluster Dialog - Shows list of prospects in cluster */}
